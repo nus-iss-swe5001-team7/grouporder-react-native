538be6e3139ba8edbc98f50853fb9099b529309a
angts <ts.ang.dvlpr@gmail.com>
2024-10-19 15:40:28 +0800

    Update projUrl.ts to include the AWS URL

--- constants/projUrl.ts
+++ constants/projUrl.ts
@@ -1,15 +1,15 @@
 // constants/projUrl.ts
 
-export let projEnv: 'development' | 'production' = 'development'; // Declare specific environment types
+export let projEnv = 'production'; // Declare specific environment types
 export let projUrl: string = ''; // Initialize projUrl with a default value
 
 if (projEnv === 'development') {
     //Remote Mock endpoint
-    projUrl = "https://0vl43.wiremockapi.cloud";
+    // projUrl = "https://0vl43.wiremockapi.cloud";
     //Local microservice endpoint, change to your dev pc own local ip
-    // projUrl = "http://192.168.50.148:8765";
+    projUrl = "http://192.168.50.148:8765";
 } else if (projEnv === 'production') {
-    projUrl = "https://aws02161231237123638.com";
+    projUrl = "http://group-order-lb-621478777.ap-southeast-1.elb.amazonaws.com";
 }
 
 export default projUrl;
